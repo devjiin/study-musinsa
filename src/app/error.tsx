d6d850a24cf3bb7d5b4ce811872d0a20481a2457
devjiin <2ddung@naver.com>
2025-07-28 17:36:16 +0900

    Feat: notion api 연동

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,4 @@
-
-"use client"
+"use client";
 export default function Error() {
-  return (
-   <>dd</>
-  )
+  return <>error!</>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
 import ContentTabs from "@/components/ContentTabs/ContentTabs";
+import { getData } from "../../notion";
+
+const pageId = "239427665b958058bc76e92c8e27e2df";
+
+const ItemCardApi = async () => {
+  console.log(pageId);
+  const data = await getData(pageId);
+
+  return <main></main>;
+};
 
 export default function Home() {
-  return <ContentTabs />;
+  return (
+    <>
+      <ItemCardApi />
+      <ContentTabs />
+    </>
+  );
 }
